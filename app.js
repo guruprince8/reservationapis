6f7a7a381f2b6cbba2ac137f6236931f8819ffd5
guruprince8 <guru.prince8@gmail.com>
2022-01-11 01:07:26 +0530

    tours model

--- app.js
+++ app.js
@@ -6,67 +6,100 @@ const app = express();
 
 const port = 3000;
 
+mongoose.connect(url,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true 
+    // useCreateIndex:true,
+    // useFindAndModfiy:false
+}).then(con => {
+    //console.log(con.connections);
+    console.log('Connetion successful');
+}).catch(err => {
+    console.log(err);
+});
+
 /******* resevervation apis */
 
+/*  Start - Schema and Model */
+const resverationsDB = mongoose.connection.useDb('reservations');
+const tourSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true,'A tour must have price']
+    }
+});
+
+const Tour = resverationsDB.model('Tour',tourSchema);
+
+/*** End -  Schema and Model */
+
 /** hotel api's */
-
 app.get('/api/v1/hotels',(req,res)=>{
-
-    mongoose.connect(url,{
-        useNewUrlParser:true,
-        useUnifiedTopology: true 
-        // useCreateIndex:true,
-        // useFindAndModfiy:false
-    }).then(con => {
-        console.log(con.connections);
-        console.log('Connetion successful');
-    }).catch(err => {
-        console.log(err);
-    });
-
-
 });
 
 app.post('/api/v1/hotels',(req,res)=>{
-
-});
-
-
-/** tour api's */
-
-app.get('/api/v1/tours',(req,res)=>{
-
-    mongoose.connect(url,{
-        useNewUrlParser:true,
-        useUnifiedTopology: true 
-        // useCreateIndex:true,
-        // useFindAndModfiy:false
-    }).then(con => {
-        console.log(con.connections);
-        console.log('Connetion successful');
-    }).catch(err => {
-        console.log(err);
-    });
-
-
 });
 
 /** tour api's */
 
-app.get('/api/v1/tours',(req,res)=>{
+app.get('/api/v1/tours',async (req,res)=>{
+   try {
+   const tours = await Tour.find();
+   //console.log(tours);
+   res.status(200).json({
+        "status":"success",
+        "length":tours.length,
+        "data":tours
+   });
+ } catch(err){
+    console.log(err);
+   }
 
-    mongoose.connect(url,{
-        useNewUrlParser:true,
-        useUnifiedTopology: true 
-        // useCreateIndex:true,
-        // useFindAndModfiy:false
-    }).then(con => {
-        console.log(con.connections);
-        console.log('Connetion successful');
+    
+});
+
+
+app.get('/api/v1/tours/:id',async (req,res)=>{
+    try {
+    const tours = await Tour.findById(req.params.id);
+    //console.log(tours);
+    res.status(200).json({
+         "status":"success",
+        //  "length":tours.length,
+         "data":tours
+    });
+  } catch(err){
+     console.log(err);
+    }
+ 
+     
+ });
+
+app.post('/api/v1/tours',(req,res)=>{
+    const testTour = new Tour ({
+        name:'The Park Capmer',
+        rating: 4.7,
+        price: 497.0
+    });
+    testTour.save().then(doc => {
+        console.log(doc);
     }).catch(err => {
         console.log(err);
     });
+});
 
+/** user  api's */
+
+app.get('/api/v1/users',(req,res)=>{
 
 });
 
